42d379348f87c2bd0b4fcaaa93001c049780e29e
Sergei Driga <drigaserge@gmail.com>
2023-01-29 22:18:58 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,122 @@
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        task0();
+        task1();
+        task2();
+        task3();
+    }
+    public static void printSeparator() {
+        System.out.println("+++++++++++++++++");
+        System.out.println("-----------------");
+    }
+
+    public static void printIssues(int issueCount) {
+        System.out.println(issueCount);
+    }
+
+    public static int sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+
+    }
+
+    public static void task0 () {
+        System.out.println("Task 0, theory");
+        int[] issuesByMonth = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
+        printSeparator();
+        for (int i = 0; i < issuesByMonth.length; i++) {
+            printIssues(issuesByMonth[i]);
+            if ((i+1) % 3 ==0) {
+                printSeparator();
+            }
+        }
+        printSeparator();
+        int total = sum(issuesByMonth);
+        System.out.println(total);
+    }
+
+
+
+
+
+
+    public static void printYear(int year, boolean check) {
+        if (check) {
+            System.out.println("The year " + year + " is a leap year");
+        } else {
+            System.out.println("The year " + year + " is a not leap year");
+        }
+    }
+    public static boolean checkYear(int year) {
+        boolean isLeap;
+        isLeap = false;
+        if (year % 4 == 0 || year % 100 == 0 && year % 400 == 0) {
+            isLeap = true;
+            return isLeap;
+        } else {
+            return isLeap;
+        }
+     }
+
+    public static void task1 () {
+        System.out.println("\nTask 1");
+        int year = 2101; // year to be checked
+        boolean isLeap = checkYear(year);
+        printYear(year, isLeap);
+    }
+
+
+
+
+
+
+
+
+    public static void getMessageIOS (int currentYear, int clientOS) {
+        if (clientOS == 0 && currentYear >= 2015) {
+            System.out.println("Download the app for iOS, following the link\n");
+        } else if (clientOS == 0 && currentYear < 2015) {
+            System.out.println("Download the light version of the app for iOS, following the link\n");
+        } else if (clientOS == 1 && currentYear >= 2015) {
+            System.out.println("Download the app for Android, following the link\n");
+        } else {
+            System.out.println("Download the light version of the app for Android, following the link\n");
+        }
+    }
+
+
+    public static void task2 () {
+        System.out.println("\nTask 2");
+        int clientOS = 1; //type of software 1 - android, 0 - iOS
+        int currentYear = LocalDate.now().getYear();
+        getMessageIOS(currentYear, clientOS);
+
+    }
+
+
+
+
+
+
+
+
+    public static void task3 () {
+        System.out.println("\nTask 3");
+        int deliveryDistance = 25; // distance from the main office [km]
+        if (deliveryDistance > 100) {
+            System.out.println("No delivery available\n");
+        } else if (deliveryDistance > 60) {
+            System.out.println("Delivery will take 3 days\n");
+        } else if (deliveryDistance > 20) {
+            System.out.println("Delivery will take 2 days\n");
+        } else {
+            System.out.println("Delivery will take 1 day\n");
+        }
+    }
+}
